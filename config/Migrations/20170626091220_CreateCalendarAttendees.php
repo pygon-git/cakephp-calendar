1ab16f021ea9307a05fee332560a20dc1d970f95
Andrey Vystavkin <jujav4ik@gmail.com>
2017-06-26 12:26:53 +0300

    Adding Calendar Attendees schema for the plugin (task #4064)

---
+++ config/Migrations/20170626091220_CreateCalendarAttendees.php
@@ -0,0 +1,58 @@
+<?php
+use Migrations\AbstractMigration;
+use Phinx\Db\Adapter\MysqlAdapter;
+
+class CreateCalendarAttendees extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('calendar_attendees', ['id' => false, 'primary_key' => ['id']]);
+        $table->addColumn('id', 'uuid', [
+            'default' => null,
+            'null' => false,
+        ]);
+
+        $table->addColumn('calendar_event_id', 'uuid', [
+            'default' => null,
+            'null' => false,
+        ]);
+
+        $table->addColumn('source', 'string', [
+            'default' => null,
+            'null' => true,
+        ]);
+
+        $table->addColumn('source_id', 'string', [
+            'default' => null,
+            'null' => true,
+        ]);
+
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+
+        $table->addColumn('trashed', 'datetime', [
+            'default' => null,
+            'null' => true,
+        ]);
+
+        $table->addPrimaryKey([
+            'id',
+        ]);
+
+        $table->create();
+    }
+}
