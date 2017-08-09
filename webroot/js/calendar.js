34cf71792ffbaa71b7814ce6763543b12b129c3f
Andrey Vystavkin <jujav4ik@gmail.com>
2017-08-09 17:45:45 +0300

    Added more spacing for the days of the week (task #4260)

--- webroot/js/calendar.js
+++ webroot/js/calendar.js
@@ -156,19 +156,19 @@ Vue.component('input-checkboxes', {
     template: `
          <div class="form-group">
             <label v-for="item in options">
-                <input type="checkbox" v-model="values" :value="item.value"/>{{item.label}}
+                <span style="padding:5px;"> <input type="checkbox" v-model="values" :value="item.value"/> {{item.label}}</span>
             </label>
         </div>`,
     data() {
         return {
             options: [
-                {label: 'MO', value: 'MO'},
-                {label: 'TU', value: 'TU'},
-                {label: 'WE', value: 'WE'},
-                {label: 'TH', value: 'TH'},
-                {label: 'FR', value: 'FR'},
-                {label: 'SA', value: 'SA'},
-                {label: 'SU', value: 'SU'}
+                {label: 'Monday', value: 'MO'},
+                {label: 'Tuesday', value: 'TU'},
+                {label: 'Wednesday', value: 'WE'},
+                {label: 'Thursday', value: 'TH'},
+                {label: 'Friday', value: 'FR'},
+                {label: 'Saturday', value: 'SA'},
+                {label: 'Sunday', value: 'SU'}
             ],
             values: [],
         };
