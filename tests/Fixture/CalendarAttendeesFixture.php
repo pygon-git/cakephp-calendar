a71c4a87ee7a47048f8e162d6793d17ee8690a56
Andrey Vystavkin <jujav4ik@gmail.com>
2017-08-10 17:03:15 +0300

    Filling more-less real data for attendees

--- tests/Fixture/CalendarAttendeesFixture.php
+++ tests/Fixture/CalendarAttendeesFixture.php
@@ -43,13 +43,13 @@ class CalendarAttendeesFixture extends TestFixture
     public $records = [
         [
             'id' => '12613e81-aa1b-4c59-9eb4-e4016ac47aef',
-            'display_name' => 'Lorem ipsum dolor sit amet',
-            'contact_details' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
-            'source' => 'Lorem ipsum dolor sit amet',
-            'source_id' => 'Lorem ipsum dolor sit amet',
+            'display_name' => 'John Doe',
+            'contact_details' => 'john@example.com',
+            'source' => null,
+            'source_id' => null,
             'created' => '2017-06-26 14:54:38',
             'modified' => '2017-06-26 14:54:38',
-            'trashed' => '2017-06-26 14:54:38'
+            'trashed' => null,
         ],
     ];
 }
