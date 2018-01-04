35e54989fe98e94044436d679cf5be7a30f7c4e6
Andrey Vystavkin <jujav4ik@gmail.com>
2018-01-04 14:10:21 +0200

    Fixing phpstan typehints for sync methods (task #4989)

--- src/Model/Table/CalendarsTable.php
+++ src/Model/Table/CalendarsTable.php
@@ -251,7 +251,7 @@ class CalendarsTable extends Table
     /**
      * Synchronize calendar events
      *
-     * @param \Model\Entity\Calendar $calendar instance from the db
+     * @param object $calendar instance from the db
      * @param array $data with extra configs
      *
      * @return array $result with events responses.
@@ -305,7 +305,7 @@ class CalendarsTable extends Table
     /**
      * Synchronize calendar events
      *
-     * @param \Model\Entity\Calendar $calendar instance from the db
+     * @param object $calendar instance from the db
      * @param array $options with extra configs
      *
      * @return array $result with events responses.
